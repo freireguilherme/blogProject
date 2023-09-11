f0ccb0cd1f10e75952adb5863ec32af7e87f2592
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-09-11 11:00:57 -0300

    add: ViewModel for Add a tag request, the view for adding a tag and the controller for adding a tag

---
+++ Controllers/AdminTagsController.cs
@@ -0,0 +1,22 @@
+﻿using Blog.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult SubmitTag(AddTagRequest addTagRequest)
+        {
+            
+
+            return View("Add");
+        }
+    }
+}
