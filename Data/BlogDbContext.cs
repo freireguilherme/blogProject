f957d32cdba2a6b01202b4321e20cdaf0af50265
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-09-11 11:33:57 -0300

    add:Bd contection, Entity Framework, BlogPost and Tag Model

---
+++ Data/BlogDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Blog.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Web.Data
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts{ get; set; }
+        public DbSet<Tag> Tags{ get; set; }
+    }
+}
