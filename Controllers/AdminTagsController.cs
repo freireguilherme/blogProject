418962a63ab2e8132ae49600b0d526192a098e0a
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-09-11 12:19:12 -0300

    add:BD context and Saving a new tag

--- Controllers/AdminTagsController.cs
+++ Controllers/AdminTagsController.cs
@@ -1,20 +1,39 @@
-﻿using Blog.Web.Models.ViewModels;
+﻿using Blog.Web.Data;
+using Blog.Web.Models.Domain;
+using Blog.Web.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Web.Controllers
 {
     public class AdminTagsController : Controller
     {
+        private readonly BlogDbContext blogDbContext;
+
+        public AdminTagsController(BlogDbContext blogDbContext)
+        {
+            this.blogDbContext = blogDbContext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
             return View();
         }
+
         [HttpPost]
         [ActionName("Add")]
         public IActionResult SubmitTag(AddTagRequest addTagRequest)
         {
-            
+            // mapping AddTagRequest to Tag domain model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName
+
+            };
+
+            blogDbContext.Tags.Add(tag);
+            blogDbContext.SaveChanges();
 
             return View("Add");
         }
