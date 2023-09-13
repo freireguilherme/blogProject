5d91620f632c6c4441bb6e583b69343cbf0678fa
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-09-13 15:44:16 -0300

    add: blog posts controller and first view, add a new blog post

---
+++ Controllers/AdminBlogPostsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Controllers;
+
+public class AdminBlogPostsController : Controller
+{
+    public  IActionResult Add()
+    {
+        return View();
+    }
+}
