7a2f87b59e9f78783485807f27504819293d154a
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-09-13 17:29:36 -0300

    add: injection of tagRepository, add function to return tags to view and add function to save a new blog post (needs implementation)

--- Controllers/AdminBlogPostsController.cs
+++ Controllers/AdminBlogPostsController.cs
@@ -1,11 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog.Web.Models.ViewModels;
+using Blog.Web.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Blog.Web.Controllers;
 
 public class AdminBlogPostsController : Controller
 {
-    public  IActionResult Add()
+    private readonly ITagRepository _tagRepository;
+
+    public AdminBlogPostsController(ITagRepository tagRepository)
     {
-        return View();
+        _tagRepository = tagRepository;
+    }
+    [HttpGet]
+    public async  Task<IActionResult> Add()
+    {
+        //get tags from repository
+        var tags = await _tagRepository.GetAllAsync();
+        var model = new AddBlogPostRequest
+        {
+            Tags = tags.Select(x => new SelectListItem { Text = x.Name, Value = x.Id.ToString() })
+        };
+        
+        return View(model);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add(AddBlogPostRequest addBlogPostRequest)
+    {
+        
+        return RedirectToAction("Add");
     }
 }
