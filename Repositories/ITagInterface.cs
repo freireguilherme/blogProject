3da29d175106507d8045cd445441b02b683fa1a1
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-09-12 21:11:34 -0300

    add: Repositories pattern. The acess to db its only through bdcontext, and crud functions through a interface and repository.

---
+++ Repositories/ITagInterface.cs
@@ -0,0 +1,14 @@
+﻿using Blog.Web.Models.Domain;
+
+namespace Blog.Web.Repositories
+{
+    public interface ITagInterface
+    {
+        Task <IEnumerable<Tag>> GetAllAsync();
+        Task<Tag?> GetAsync(Guid id);
+        Task<Tag> AddAsync(Tag tag);
+        Task<Tag?> UpdateAsync(Tag tag);
+        Task<Tag?> DeleteAsync(Guid id);
+
+    }
+}
---
+++ Repositories/TagRepository.cs
@@ -0,0 +1,32 @@
+﻿using Blog.Web.Models.Domain;
+
+namespace Blog.Web.Repositories
+{
+    public class TagRepository : ITagInterface
+    {
+        public Task<Tag> AddAsync(Tag tag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tag?> DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Tag>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tag?> GetAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tag?> UpdateAsync(Tag tag)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
