1f47daef35ff056da02313061edfadbe9a11943e
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-09-14 16:39:41 -0300

    add: Model view for dealing with a editing request for a blogpost; Controller's edit functionality to correctly show on view.

--- Controllers/AdminBlogPostsController.cs
+++ Controllers/AdminBlogPostsController.cs
@@ -48,7 +48,7 @@ public class AdminBlogPostsController : Controller
 
         var selectedTags = new List<Tag>();
         //map tags from selected tags
-        foreach (var selectedTadId in addBlogPostRequest.SelectedTag)
+        foreach (var selectedTadId in addBlogPostRequest.SelectedTags)
         {
             var selectedTagIdAsGuid = Guid.Parse(selectedTadId);
             var existingTag = await _tagRepository.GetAsync(selectedTagIdAsGuid);
@@ -72,4 +72,40 @@ public class AdminBlogPostsController : Controller
         var blogPosts = await _blogPostRepository.GetAllAsync();
         return View(blogPosts);
     }
+
+    [HttpGet]
+    public async Task<IActionResult> Edit(Guid id)
+    {
+        var blogPost = await _blogPostRepository.GetAsync(id);
+        var tagsDomainModel = await _tagRepository.GetAllAsync();
+        if (blogPost != null)
+        {
+            //map the domain model into the view model
+            var model = new EditBlogPostRequest
+            {
+                Id = blogPost.Id,
+                Heading = blogPost.Heading,
+                PageTitle = blogPost.PageTitle,
+                Content = blogPost.Content,
+                ShortDescription = blogPost.ShortDescription,
+                FeaturedImageUrl = blogPost.FeaturedImageUrl,
+                UrlHandle = blogPost.UrlHandle,
+                PublishedDate = blogPost.PublishedDate,
+                Author = blogPost.Author,
+                Visible = blogPost.Visible,
+                Tags = tagsDomainModel.Select(x => new SelectListItem
+                {
+                    //<select> is receiving two values, Text and Value to display onto view
+                    Text = x.Name, Value = x.Id.ToString()
+                }),
+                SelectedTags = blogPost.Tags.Select( x=> x.Id.ToString()).ToArray()
+                
+            };
+                
+            return View(model);
+        }
+
+        return View(null);
+    }
+    
 }
