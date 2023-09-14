d277e9458d4c5b0022ddbd45ad698e74fec71106
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-09-13 21:30:19 -0300

    add: interface IBlogPostRepository and your methods on BlogPostRepository. AddAsync implemented

---
+++ Repositories/BlogPostRepository.cs
@@ -0,0 +1,41 @@
+﻿using Blog.Web.Data;
+using Blog.Web.Models.Domain;
+
+namespace Blog.Web.Repositories;
+
+public class BlogPostRepository : IBlogPostRepository
+{
+    private readonly BlogDbContext _blogDbContext;
+
+    public BlogPostRepository(BlogDbContext blogDbContext)
+    {
+        _blogDbContext = blogDbContext;
+    }
+    public Task<IEnumerable<BlogPost>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<BlogPost?> GetAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<BlogPost> AddAsync(BlogPost blogPost)
+    {
+        await _blogDbContext.AddAsync(blogPost);
+        await _blogDbContext.SaveChangesAsync();
+
+        return blogPost;
+    }
+
+    public Task<BlogPost?> UpdateAsync(BlogPost blog)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<BlogPost?> DeleteAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Repositories/IBlogPostRepository.cs
@@ -0,0 +1,12 @@
+﻿using Blog.Web.Models.Domain;
+
+namespace Blog.Web.Repositories;
+
+public interface IBlogPostRepository
+{
+    Task<IEnumerable<BlogPost>> GetAllAsync();
+    Task<BlogPost?> GetAsync(Guid id);
+    Task<BlogPost> AddAsync(BlogPost blog);
+    Task<BlogPost?> UpdateAsync(BlogPost blog);
+    Task<BlogPost?> DeleteAsync(Guid id);
+}
