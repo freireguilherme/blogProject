b24b79de698a710feea6a1ea2e642b18bdd44392
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-09-15 15:43:14 -0300

    add: we inject the image repository (interfaced) and used onto ImagesController. this way as we send a post request we can save the image on cloud and receive a url back

--- Controllers/ImagesController.cs
+++ Controllers/ImagesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net;
+using Blog.Web.Repositories;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Web.Controllers
 {
@@ -6,10 +8,24 @@ namespace Blog.Web.Controllers
     [ApiController]
     public class ImagesController : ControllerBase
     {
+        private readonly IImageRepository _imageRepository;
+
+        public ImagesController(IImageRepository imageRepository)
+        {
+            this._imageRepository = imageRepository;
+        }
         [HttpPost]
         public async Task<IActionResult> UploadAsync(IFormFile formFile)
         {
             // call a repository
+            var imageUrl = await _imageRepository.UploadAsync(formFile);
+
+            if (imageUrl == null)
+            {
+                return Problem("Something went wrong", null, (int)HttpStatusCode.InternalServerError);
+            }
+
+            return new JsonResult(new { link = imageUrl });
         }
     }
     
