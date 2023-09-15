c963b66b6425bb04941023e1ab6d38b4e698ae68
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-09-15 13:36:55 -0300

    add: upload functionality to cloudinary. We use the connection configuration on appSettings, then upload the file and expects a his url

--- Repositories/CloudinaryImageRepository.cs
+++ Repositories/CloudinaryImageRepository.cs
@@ -1,9 +1,39 @@
-﻿namespace Blog.Web.Repositories;
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+
+namespace Blog.Web.Repositories;
 
 public class CloudinaryImageRepository : IImageRepository
 {
-    public Task<string> UploadAsync(IFormFile formFile)
+    private readonly IConfiguration _configuration;
+    private readonly Account _account;
+    public CloudinaryImageRepository(IConfiguration configuration)
     {
-        throw new NotImplementedException();
+        this._configuration = configuration;
+        _account = new Account(
+            configuration.GetSection("Cloudinary")["CloudName"],
+            configuration.GetSection("Cloudinary")["ApiKey"],
+            configuration.GetSection("Cloudinary")["ApiSecret"]
+            );
+    }
+
+    public async Task<string> UploadAsync(IFormFile formFile)
+    {
+        var client = new Cloudinary(_account);
+
+        var uploadParams = new ImageUploadParams()
+        {
+            File = new FileDescription(formFile.FileName, formFile.OpenReadStream()),
+            DisplayName = formFile.FileName
+        };
+
+        var uploadResult = await client.UploadAsync(uploadParams);
+
+        if (uploadParams != null && uploadResult.StatusCode == System.Net.HttpStatusCode.OK)
+        {
+            return uploadResult.SecureUrl.ToString();
+        }
+
+        return null;
     }
 }
