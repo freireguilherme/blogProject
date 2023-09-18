8d0d2aaccfdacacc3436f9d0dc773055e5612f86
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-09-18 13:07:50 -0300

    feat: add BlogsController.cs to return a blogPost by your urlHandle

---
+++ Controllers/BlogsController.cs
@@ -0,0 +1,20 @@
+﻿using Blog.Web.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Controllers;
+
+public class BlogsController : Controller
+{
+    private readonly IBlogPostRepository _blogPostRepository;
+
+    public BlogsController(IBlogPostRepository blogPostRepository)
+    {
+        _blogPostRepository = blogPostRepository;
+    }
+    [HttpGet]
+    public async Task<IActionResult> Index(string urlHandle)
+    {
+        var blogPost = await _blogPostRepository.GetByUrlHandleAsync(urlHandle);
+        return View(blogPost);
+    }
+}
