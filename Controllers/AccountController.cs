a89c89f096c53cb71df27d9efdfd57a6d27b95a6
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-09-18 18:31:37 -0300

    feat: a new controller to manage Users accounts. We added Register get and post methods

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using Blog.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Controllers;
+
+public class AccountController : Controller
+{
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public AccountController(UserManager<IdentityUser> userManager)
+    {
+        _userManager = userManager;
+    }
+    
+    [HttpGet]
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+    {
+        var identityUser = new IdentityUser
+        {
+            UserName = registerViewModel.Username,
+            Email = registerViewModel.Email,
+        };
+        var identityResult = await _userManager.CreateAsync(identityUser, registerViewModel.Password);
+
+        if (identityResult.Succeeded)
+        {
+            var roleIdentityResult = await _userManager.AddToRoleAsync(identityUser, "User");
+
+            if (roleIdentityResult.Succeeded)
+            {
+                return RedirectToAction("Register");
+            }
+        }
+
+        return View();
+    }
+}
