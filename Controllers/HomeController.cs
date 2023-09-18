b43f5a3101fcc04039d510a48b18921a0e70ade9
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-09-18 13:14:51 -0300

    feat: add tagRepository to index method to return all tags (in order to display them of course).

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,16 +9,26 @@ namespace Blog.Web.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IBlogPostRepository _blogPostRepository;
+        private readonly ITagRepository _tagRepository;
 
-        public HomeController(ILogger<HomeController> logger, IBlogPostRepository blogPostRepository)
+        public HomeController(ILogger<HomeController> logger,
+            IBlogPostRepository blogPostRepository,
+            ITagRepository tagRepository)
         {
             _logger = logger;
             _blogPostRepository = blogPostRepository;
+            _tagRepository = tagRepository;
         }
 
         public async Task<IActionResult> Index()
         {
+            //get all blogs
             var blogPosts = await _blogPostRepository.GetAllAsync();
+            
+            //get all tags
+            var tags = await _tagRepository.GetAllAsync();
+            
+            //TODO but how to display two sets of data on one view?
             return View(blogPosts);
         }
 
