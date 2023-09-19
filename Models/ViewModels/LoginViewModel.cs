d57113d78e9c584b57b3caf15b0ab330677c0451
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-09-18 23:48:18 -0300

    feat(models): add LoginViewModel

---
+++ Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Blog.Web.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
