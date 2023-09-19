af9cc1a519f9d4f62d003f47e578c4dbe580482a
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-09-19 18:24:05 -0300

    feat: add totalLikes variable who gets the likes from the repository and then mapped the blogPost domain model to blogDetails view model with the total likes info.

--- Controllers/BlogsController.cs
+++ Controllers/BlogsController.cs
@@ -1,4 +1,5 @@
-﻿using Blog.Web.Repositories;
+﻿using Blog.Web.Models.ViewModels;
+using Blog.Web.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Web.Controllers;
@@ -6,15 +7,40 @@ namespace Blog.Web.Controllers;
 public class BlogsController : Controller
 {
     private readonly IBlogPostRepository _blogPostRepository;
+    private readonly IBlogPostLikeRepository _blogPostLikeRepository;
 
-    public BlogsController(IBlogPostRepository blogPostRepository)
+    public BlogsController(IBlogPostRepository blogPostRepository,
+        IBlogPostLikeRepository blogPostLikeRepository)
     {
         _blogPostRepository = blogPostRepository;
+        _blogPostLikeRepository = blogPostLikeRepository;
     }
     [HttpGet]
     public async Task<IActionResult> Index(string urlHandle)
     {
         var blogPost = await _blogPostRepository.GetByUrlHandleAsync(urlHandle);
-        return View(blogPost);
+        var blogPostLikeViewModel = new BlogDetailsViewModel();
+       
+
+        if (blogPost != null)
+        {
+            var totalLikes = await _blogPostLikeRepository.GetTotalLikes(blogPost.Id);
+             blogPostLikeViewModel = new BlogDetailsViewModel
+            {
+                Id = blogPost.Id,
+                Heading = blogPost.Heading,
+                PageTitle = blogPost.PageTitle,
+                Content = blogPost.Content,
+                ShortDescription = blogPost.ShortDescription,
+                FeaturedImageUrl = blogPost.FeaturedImageUrl,
+                UrlHandle = blogPost.UrlHandle,
+                PublishedDate = blogPost.PublishedDate,
+                Author = blogPost.Author,
+                Visible = blogPost.Visible,
+                Tags = blogPost.Tags,
+                TotalLikes = totalLikes
+            };
+        }
+        return View(blogPostLikeViewModel);
     }
 }
