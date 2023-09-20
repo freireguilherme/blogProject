09a419a3dbe03e8b0fef788ab23f76a9f0e5d64a
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-09-20 11:59:15 -0300

    feat(api controller): add a controller to handle a Like request: we map the view model into domain model, await the repository to save the changes in the database and return Ok. Needs some validations (one blogPostLike per user)

---
+++ Controllers/BlogPostLikeController.cs
@@ -0,0 +1,35 @@
+﻿using Blog.Web.Models.Domain;
+using Blog.Web.Models.ViewModels;
+using Blog.Web.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogPostLikeController : Controller
+    {
+        private readonly IBlogPostLikeRepository _blogPostLikeRepository;
+
+        public BlogPostLikeController(IBlogPostLikeRepository blogPostLikeRepository)
+        {
+            _blogPostLikeRepository = blogPostLikeRepository;
+        }
+        [HttpPost]
+        [Route("Add")]
+        public async Task<IActionResult> AddLike([FromBody] AddLikeRequest addLikeRequest)
+        {
+            var model = new BlogPostLike
+            {
+                BlogPostId = addLikeRequest.BlogPostId,
+                UserId = addLikeRequest.UserId
+            };
+            
+            await _blogPostLikeRepository.AddLikeForBlog(model);
+
+            return Ok();
+        }
+    }
+    
+}
+
