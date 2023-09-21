7f23053b33e49ce06afe8b5cbb17415e4596ee43
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-09-21 13:17:45 -0300

    feat(controller): in the controller we added signInManager and userManger to the constructor. Then is the user is SignIn, we check if he has already liked the blogPost and map this info to the view model.

--- Controllers/BlogsController.cs
+++ Controllers/BlogsController.cs
@@ -1,5 +1,6 @@
 ﻿using Blog.Web.Models.ViewModels;
 using Blog.Web.Repositories;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Web.Controllers;
@@ -8,16 +9,23 @@ public class BlogsController : Controller
 {
     private readonly IBlogPostRepository _blogPostRepository;
     private readonly IBlogPostLikeRepository _blogPostLikeRepository;
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly UserManager<IdentityUser> _userManager;
 
     public BlogsController(IBlogPostRepository blogPostRepository,
-        IBlogPostLikeRepository blogPostLikeRepository)
+        IBlogPostLikeRepository blogPostLikeRepository,
+        SignInManager<IdentityUser> signInManager,
+        UserManager<IdentityUser> userManager)
     {
         _blogPostRepository = blogPostRepository;
         _blogPostLikeRepository = blogPostLikeRepository;
+        _signInManager = signInManager;
+        _userManager = userManager;
     }
     [HttpGet]
     public async Task<IActionResult> Index(string urlHandle)
     {
+        var liked = false;
         var blogPost = await _blogPostRepository.GetByUrlHandleAsync(urlHandle);
         var blogPostLikeViewModel = new BlogDetailsViewModel();
        
@@ -25,7 +33,21 @@ public class BlogsController : Controller
         if (blogPost != null)
         {
             var totalLikes = await _blogPostLikeRepository.GetTotalLikes(blogPost.Id);
-             blogPostLikeViewModel = new BlogDetailsViewModel
+
+            if (_signInManager.IsSignedIn(User))
+            {
+                //get like for this blog for this user
+                var likesForBlog = await _blogPostLikeRepository.GetLikesForBlog(blogPost.Id);
+                var userId = _userManager.GetUserId(User);
+
+                if (userId != null)
+                {
+                    var likeFromUser = likesForBlog.FirstOrDefault(x => x.UserId == Guid.Parse(userId));
+                    liked = likeFromUser != null;
+                }
+            }
+            
+            blogPostLikeViewModel = new BlogDetailsViewModel
             {
                 Id = blogPost.Id,
                 Heading = blogPost.Heading,
@@ -38,7 +60,8 @@ public class BlogsController : Controller
                 Author = blogPost.Author,
                 Visible = blogPost.Visible,
                 Tags = blogPost.Tags,
-                TotalLikes = totalLikes
+                TotalLikes = totalLikes,
+                Liked = liked
             };
         }
         return View(blogPostLikeViewModel);
