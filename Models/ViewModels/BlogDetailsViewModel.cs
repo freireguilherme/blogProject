21578817657bc2d9670b212dfa7227d41b93dfba
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-09-21 13:13:54 -0300

    feat: add a new property: if the blog is Liked

--- Models/ViewModels/BlogDetailsViewModel.cs
+++ Models/ViewModels/BlogDetailsViewModel.cs
@@ -16,4 +16,5 @@ public class BlogDetailsViewModel
     public bool Visible { get; set; }
     public ICollection<Tag> Tags { get; set; }
     public int TotalLikes { get; set; }
+    public bool Liked { get; set; } 
 }
