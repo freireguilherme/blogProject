ae1de3c8651f810323d1b494bde3f1e1b3b2e214
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-09-22 21:42:03 -0300

    feat: now Blog Details view model also has a list of blogComments

--- Models/ViewModels/BlogDetailsViewModel.cs
+++ Models/ViewModels/BlogDetailsViewModel.cs
@@ -17,5 +17,7 @@ public class BlogDetailsViewModel
     public ICollection<Tag> Tags { get; set; }
     public int TotalLikes { get; set; }
     public bool Liked { get; set; }
+
     public string CommentDescription { get; set; }
+    public IEnumerable<BlogComment> Comments { get; set; }
 }
