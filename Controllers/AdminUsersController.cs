bdb03dfd79da7779a31a1da184d0994dff415eb0
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-09-24 11:09:35 -0300

    feat(controller): added a AdminUserController and a List method to display all users.

---
+++ Controllers/AdminUsersController.cs
@@ -0,0 +1,36 @@
+﻿using Blog.Web.Models.ViewModels;
+using Blog.Web.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Controllers;
+
+[Authorize(Roles = "Admin")]
+public class AdminUsersController : Controller
+{
+    private readonly IUserRepository _userRepository;
+
+    public AdminUsersController(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+    
+    public async Task<IActionResult> List()
+    {
+        var users = await _userRepository.GetAll();
+        var usersViewModel = new UserViewModel
+        {
+            Users = new List<User>()
+        };
+        foreach (var user in users)
+        {
+            usersViewModel.Users.Add(new Models.ViewModels.User
+            {
+                Id = Guid.Parse(user.Id),
+                Username = user.UserName,
+                EmailAddress = user.Email
+            });
+        }
+        return View(usersViewModel);
+    }
+}
