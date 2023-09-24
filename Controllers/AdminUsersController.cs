df62d1da0c066bc4a49e9edd5dd8e345125eed3b
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-09-24 13:04:05 -0300

    feat(controller): in the AdminUsersController we added a List asign with a post method, so we can add a new user, assign him with the appropriate roles.

--- Controllers/AdminUsersController.cs
+++ Controllers/AdminUsersController.cs
@@ -1,6 +1,7 @@
 ﻿using Blog.Web.Models.ViewModels;
 using Blog.Web.Repositories;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Web.Controllers;
@@ -9,12 +10,15 @@ namespace Blog.Web.Controllers;
 public class AdminUsersController : Controller
 {
     private readonly IUserRepository _userRepository;
+    private readonly UserManager<IdentityUser> _userManager;
 
-    public AdminUsersController(IUserRepository userRepository)
+    public AdminUsersController(IUserRepository userRepository,
+        UserManager<IdentityUser> userManager)
     {
         _userRepository = userRepository;
+        _userManager = userManager;
     }
-    
+    [HttpGet]
     public async Task<IActionResult> List()
     {
         var users = await _userRepository.GetAll();
@@ -32,5 +36,36 @@ public class AdminUsersController : Controller
             });
         }
         return View(usersViewModel);
+    }
+    [HttpPost]
+    public async Task<IActionResult> List(UserViewModel request)
+    {
+        var identityUser = new IdentityUser
+        {
+            UserName = request.Username,
+            Email = request.EmailAddress
+        };
+
+        var identityResult = await _userManager.CreateAsync(identityUser, request.Password);
+        if(identityResult is not null)
+        {
+            if (identityResult.Succeeded)
+            {
+                //assign roles to this user
+                var roles = new List<string> { "User" };
+                if (request.AdminRoleCheckbox)
+                {
+                    roles.Add("Admin");
+                }
+
+                identityResult = await _userManager.AddToRolesAsync(identityUser, roles);
+                if (identityResult is not null && identityResult.Succeeded)
+                {
+                    return RedirectToAction("List", "AdminUsers");
+                }
+            }
+        }
+
+        return View();
     }
 }
