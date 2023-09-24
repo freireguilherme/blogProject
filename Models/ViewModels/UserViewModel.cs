a523d3c2f996b522f7e7a25a113501235f31d7f2
Guilherme Freire <guilhermemagalhaesfreire22@gmail.com>
2023-09-24 13:02:31 -0300

    feat: to add a new Create users functionality, now UserViewModel also has properties for adding a new user.

--- Models/ViewModels/UserViewModel.cs
+++ Models/ViewModels/UserViewModel.cs
@@ -3,4 +3,8 @@
 public class UserViewModel
 {
     public List<User> Users { get; set; }
+    public string Username { get; set; }
+    public string EmailAddress { get; set; }
+    public string Password { get; set; }
+    public bool AdminRoleCheckbox { get; set; }
 }
